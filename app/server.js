a1385c0eeacef137947c8aff629bd2dbed39ff13
Ryan A. Rodriguez <ryarodriguez@tesla.com>
2017-01-16 09:55:05 -0800

    Mongo integration, db connection established.

--- app/server.js
+++ app/server.js
@@ -4,14 +4,31 @@ import configManager from './infra/config-manager';
 import middlewareManager from './infra/middleware-manager';
 import routeManager from './infra/route-manager';
 import assetsManager from './infra/assets-manager';
+import mongoose from 'mongoose';
 
 const app = express();
+const url = 'mongodb://rowdy_rye:rowdy_rye@ds161038.mlab.com:61038/sensor_lake';
+
 
 configManager.handle(app);
 middlewareManager.handle(app);
 assetsManager.handle(app);
 routeManager.handle(app);
 
-app.listen(nconf.get('port'), () => {
-    console.log('Listening on http://' + nconf.get('host') + ':' + nconf.get('port'));    
+configManager.handle(app);
+middlewareManager.handle(app);
+assetsManager.handle(app);
+routeManager.handle(app);
+console.log('FUCK!!');
+
+mongoose.connect(url, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Connected to db.');
+    app.listen(nconf.get('port'), () => {
+        console.log('Listening on http://' + nconf.get('host') + ':' + nconf.get('port'));
+    });
 });
+
+
